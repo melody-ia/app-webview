a8679243e7d9e3ac70c4e8d156941ba34aa44a8a
willsoft <54517568+soo-willsoft@users.noreply.github.com>
2019-12-17 17:51:57 +0900

    V 1.0 Rebuild

---
+++ app/src/main/java/com/etbc/eos/MainActivity.java
@@ -0,0 +1,55 @@
+package com.etbc.eos;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true); // 팝업
+        webSettings.setLoadsImagesAutomatically(true); // 이미지 리소스 다운
+        webSettings.setUseWideViewPort (false); // 가로모드
+        webSettings.setSupportZoom (true); // 확대,축소
+        webSettings.setDomStorageEnabled(false); // 로컬스토리지
+        webSettings.setAppCacheEnabled (false); // 앱 캐시사용
+
+        webSettings.setAllowFileAccessFromFileURLs(true); // 파일
+        webSettings.setSaveFormData(false);
+        webSettings.setSupportMultipleWindows(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.getSettings().setUserAgentString(
+        this.webView.getSettings().getUserAgentString()
+                + " "
+                + getString(R.string.user_agent_suffix)
+        );
+        webView.loadUrl( this.getResources().getString(R.string.test) );
+
+    }
+
+    public void onBackPressed(){
+
+        if(webView.canGoBack()) webView.goBack();
+        else finish();
+    }
+}

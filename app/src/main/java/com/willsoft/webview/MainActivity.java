06ca6c13890cb1eb796e8d1c683efeb582c41a18
soo-willsoft <soo@willsoft.kr>
2020-02-07 19:55:44 +0900

    change package name willsoft

--- app/src/main/java/com/willsoft/webview/MainActivity.java
+++ app/src/main/java/com/willsoft/webview/MainActivity.java
@@ -1,12 +1,11 @@
 package com.willsoft.webview;
 
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
